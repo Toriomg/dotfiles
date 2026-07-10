781f19fac7b783609c3fd9d6579080b6a6639e7d
Toriomg <hectorusuario@gmail.com>
2026-07-10 02:44:18 +0200

    emojis

--- hypr/.config/hypr/scripts/emoji.sh
+++ hypr/.config/hypr/scripts/emoji.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-rofimoji \
-    --selector wofi \
-    --selector-args="--conf $HOME/.config/wofi/emoji --style $HOME/.config/wofi/style-emoji.css" \
-    --clipboarder wl-copy \
-    --action copy \
-    --hidden-descriptions
+
+DATA_DIR="/usr/lib/python3.14/site-packages/picker/data"
+
+chosen=$(cat "$DATA_DIR"/emojis_smileys_emotion.csv \
+             "$DATA_DIR"/emojis_people_body.csv \
+             "$DATA_DIR"/emojis_animals_nature.csv \
+             "$DATA_DIR"/emojis_food_drink.csv \
+             "$DATA_DIR"/emojis_travel_places.csv \
+             "$DATA_DIR"/emojis_activities.csv \
+             "$DATA_DIR"/emojis_objects.csv \
+             "$DATA_DIR"/emojis_symbols.csv \
+             "$DATA_DIR"/emojis_flags.csv \
+    | awk '{print $1}' \
+    | wofi --dmenu \
+           --conf "$HOME/.config/wofi/emoji" \
+           --style "$HOME/.config/wofi/style-emoji.css" \
+           --prompt "")
+
+[ -n "$chosen" ] && wl-copy "$chosen"
